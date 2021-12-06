10e5d5b01da73a2971dd9e6c4625ef7d451c8d0c
James Troughton <jamesstroughton@gmail.com>
2021-12-07 12:29:15 +1300

    ✨ Widgets

---
+++ components/Widgets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Widgets = () => {
+  return (
+    <div className='hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5'>
+      <div className='sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12 '>
+        <div className='flex items-center bg-[#202327] p-3 rounded-full relative'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Widgets;
