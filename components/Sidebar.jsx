7ec2243af83633cf78df1da9736d3da0a7a75b75
James Troughton <jamesstroughton@gmail.com>
2021-12-07 12:28:07 +1300

    ✨ Sidebar

---
+++ components/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'next/image';
+import SidebarIcons from './SidebarIcons';
+import { HomeIcon } from '@heroicons/react/outline';
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from '@heroicons/react/outline';
+
+const Sidebar = () => {
+  return (
+    <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
+      <div className='flex items-center justify-center p-0 w-14 h-14 hoverAnimation xl:ml-24'>
+        <Image src='https://rb.gy/ogau5a' width={30} height={30} />
+      </div>
+      <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+        <SidebarIcons text='Home' Icon={HomeIcon} active />
+        <SidebarIcons text='Notifications' Icon={BellIcon} />
+        <SidebarIcons text='Messages' Icon={InboxIcon} />
+        <SidebarIcons text='Bookmarks' Icon={BookmarkIcon} />
+        <SidebarIcons text='Lists' Icon={ClipboardListIcon} />
+        <SidebarIcons text='Profile' Icon={UserIcon} />
+        <SidebarIcons text='More' Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button className='hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md transition duration-400 hover:scale-[0.98] hover:bg-gradient-to-br from-[#1d9bf0] to-blue-400'>
+        Tweet
+      </button>
+      <div className='text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5'>
+        <img
+          src='https://avataaars.io/?avatarStyle=Circle&topType=WinterHat3&accessoriesType=Blank&hatColor=PastelRed&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=BlazerShirt&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Smile&skinColor=Light'
+          alt=''
+          className='h-10 w-10 rounded-full xl:mr-2.5'
+        />
+        <div className='hidden leading-5 xl:inline'>
+          <h4 className='font-bold'>James Indeed</h4>
+          <p className='text-[#6e767d]'>@jamesindeed</p>
+        </div>
+        <DotsHorizontalIcon className='hidden h-5 ml-10 xl:inline' />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
