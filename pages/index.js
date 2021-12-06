1c334f4130458929f6b523d3f6ee348f002febb2
James Troughton <jamesstroughton@gmail.com>
2021-12-07 12:28:37 +1300

    ➕ Imports

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import React from 'react';
 import { Sidebar } from '../components';
+import Feed from '../components/Feed';
+import Widgets from '../components/Widgets';
 
 export default function Home() {
   return (
@@ -9,10 +11,10 @@ export default function Home() {
         <title>Twitter</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
+      <div className='flex min-h-screen mx-auto bg-black max-w-screen-2xl'>
         <Sidebar />
-        {/* Feed */}
-        {/* Widgets 2 */}
+        <Feed />
+        <Widgets />
 
         {/* Modal */}
       </div>
