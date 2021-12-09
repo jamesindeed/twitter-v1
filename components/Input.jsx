c8d3a6b85bec6d3c9a99f20e31864270b3e91575
James Troughton <jamesstroughton@gmail.com>
2021-12-10 10:40:42 +1300

    ✨ Input Component

---
+++ components/Input.jsx
@@ -0,0 +1,127 @@
+import {
+  XIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+} from '@heroicons/react/outline';
+import React, { useState, useRef } from 'react';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
+
+const Input = () => {
+  const [input, setInput] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+  const filePickerRef = useRef(null);
+  const [showEmojis, setShowEmojis] = useState(false);
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split('-');
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push('0x' + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
+
+  return (
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+    >
+      <img
+        src='https://avataaars.io/?avatarStyle=Circle&topType=WinterHat3&accessoriesType=Blank&hatColor=PastelRed&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=BlazerShirt&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Smile&skinColor=Light'
+        alt=''
+        className='rounded-full cursor-pointer h-11 w-11'
+      />
+      <div className='w-full divide-y divide-gray-700'>
+        <div className={`${selectedFile && 'pb-7'} ${input && 'space-y-2.5'}`}>
+          <textarea
+            onChange={(e) => setInput(e.target.value)}
+            className='bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px] h-[80px] max-h-[200px]'
+            placeholder="What's happening?"
+            value={input}
+            rows={2}
+          />
+
+          {selectedFile && (
+            <div className='relative'>
+              <div
+                onClick={() => setSelectedFile(null)}
+                className='absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex item-center justify-center top-1 left-1 cursor-pointer'
+              >
+                <XIcon className='justify-center h-5 text-white mt-[5px] item-center' />
+              </div>
+              <img
+                src={selectedFile}
+                alt='selectedFile'
+                className='object-contain mt-2.5 rounded-2xl max-h-80'
+              />
+            </div>
+          )}
+        </div>
+        <div className='flex items-center justify-between pt-2.5'>
+          <div className='flex items-center'>
+            <div
+              className='icon'
+              onClick={() => {
+                filePickerRef.current.click();
+              }}
+            >
+              <PhotographIcon className='text-[#1d9bf0] h-[22px]' />
+              <input
+                type='file'
+                ref={filePickerRef}
+                hidden
+                onChange={addImageToPost}
+              />
+            </div>
+
+            <div className='rotate-90 icon'>
+              <ChartBarIcon className='text-[#1d9bf0] h-[22px]' />
+            </div>
+
+            <div className='icon' onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className='text-[#1d9bf0] h-[22px]' />
+            </div>
+
+            <div className='icon'>
+              <CalendarIcon className='text-[#1d9bf0] h-[22px]' />
+            </div>
+
+            {showEmojis && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: 'absolute',
+                  marginTop: '465px',
+                  marginLeft: -40,
+                  maxWidth: '320px',
+                  borderRadius: '20px',
+                }}
+                theme='dark'
+              />
+            )}
+          </div>
+          <button
+            className='bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default'
+            disabled={!input.trim() && !selectedFile}
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
