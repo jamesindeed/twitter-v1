ba2432abf567ce98eb2e646f57ce3ebde7c70048
James Troughton <jamesstroughton@gmail.com>
2022-01-04 16:13:16 +1300

    🔥 Firebase | Submit Post

--- components/Input.jsx
+++ components/Input.jsx
@@ -6,6 +6,15 @@ import {
   PhotographIcon,
 } from '@heroicons/react/outline';
 import React, { useState, useRef } from 'react';
+import { db, storage } from '../firebase';
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from '@firebase/firestore';
+import { getDownloadURL, ref, uploadString } from '@firebase/storage';
 import { Picker } from 'emoji-mart';
 import 'emoji-mart/css/emoji-mart.css';
 
@@ -14,6 +23,7 @@ const Input = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const filePickerRef = useRef(null);
   const [showEmojis, setShowEmojis] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const addImageToPost = (e) => {
     const reader = new FileReader();
@@ -34,9 +44,41 @@ const Input = () => {
     setInput(input + emoji);
   };
 
+  const sendPost = async (e) => {
+    if (loading) return;
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      // id: session.user.uid,
+      // username: session.user.name,
+      // userImg: session.user.image,
+      // tag: session.user.tag,
+      text: input,
+      timestamp: serverTimestamp(),
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, 'data_url').then(async () => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        });
+      });
+    }
+
+    setLoading(false);
+    setInput('');
+    setSelectedFile(null);
+    setShowEmojis(false);
+  };
+
   return (
     <div
-      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide ${
+        loading && 'opacity-60'
+      }`}
     >
       <img
         src='https://avataaars.io/?avatarStyle=Circle&topType=WinterHat3&accessoriesType=Blank&hatColor=PastelRed&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=BlazerShirt&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Smile&skinColor=Light'
@@ -69,56 +111,61 @@ const Input = () => {
             </div>
           )}
         </div>
-        <div className='flex items-center justify-between pt-2.5'>
-          <div className='flex items-center'>
-            <div
-              className='icon'
-              onClick={() => {
-                filePickerRef.current.click();
-              }}
-            >
-              <PhotographIcon className='text-[#1d9bf0] h-[22px]' />
-              <input
-                type='file'
-                ref={filePickerRef}
-                hidden
-                onChange={addImageToPost}
-              />
-            </div>
 
-            <div className='rotate-90 icon'>
-              <ChartBarIcon className='text-[#1d9bf0] h-[22px]' />
-            </div>
-
-            <div className='icon' onClick={() => setShowEmojis(!showEmojis)}>
-              <EmojiHappyIcon className='text-[#1d9bf0] h-[22px]' />
-            </div>
-
-            <div className='icon'>
-              <CalendarIcon className='text-[#1d9bf0] h-[22px]' />
-            </div>
-
-            {showEmojis && (
-              <Picker
-                onSelect={addEmoji}
-                style={{
-                  position: 'absolute',
-                  marginTop: '465px',
-                  marginLeft: -40,
-                  maxWidth: '320px',
-                  borderRadius: '20px',
+        {!loading && (
+          <div className='flex items-center justify-between pt-2.5'>
+            <div className='flex items-center'>
+              <div
+                className='icon'
+                onClick={() => {
+                  filePickerRef.current.click();
                 }}
-                theme='dark'
-              />
-            )}
+              >
+                <PhotographIcon className='text-[#1d9bf0] h-[22px]' />
+                <input
+                  type='file'
+                  ref={filePickerRef}
+                  hidden
+                  onChange={addImageToPost}
+                  onClick={() => setShowEmojis(false)}
+                />
+              </div>
+
+              <div className='rotate-90 icon'>
+                <ChartBarIcon className='text-[#1d9bf0] h-[22px]' />
+              </div>
+
+              <div className='icon' onClick={() => setShowEmojis(!showEmojis)}>
+                <EmojiHappyIcon className='text-[#1d9bf0] h-[22px]' />
+              </div>
+
+              <div className='icon'>
+                <CalendarIcon className='text-[#1d9bf0] h-[22px]' />
+              </div>
+
+              {showEmojis && (
+                <Picker
+                  onSelect={addEmoji}
+                  style={{
+                    position: 'absolute',
+                    marginTop: '465px',
+                    marginLeft: -40,
+                    maxWidth: '320px',
+                    borderRadius: '20px',
+                  }}
+                  theme='dark'
+                />
+              )}
+            </div>
+            <button
+              className='bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default'
+              disabled={!input.trim() && !selectedFile}
+              onClick={sendPost}
+            >
+              Tweet
+            </button>
           </div>
-          <button
-            className='bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default'
-            disabled={!input.trim() && !selectedFile}
-          >
-            Tweet
-          </button>
-        </div>
+        )}
       </div>
     </div>
   );
