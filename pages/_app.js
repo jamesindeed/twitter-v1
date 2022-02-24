3b8af22f70c67e007530688b34fcbb261bdff85f
James Troughton <jamesstroughton@gmail.com>
2022-02-25 12:02:46 +1300

    ⚡️ Authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import 'tailwindcss/tailwind.css';
+import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
-export default MyApp;
