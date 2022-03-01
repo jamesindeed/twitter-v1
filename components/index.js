2eba40c29898a464c77c201cfbc1fd33fecc86a1
James Troughton <jamesstroughton@gmail.com>
2022-03-01 14:21:03 +1300

    ➕ Imports

--- components/index.js
+++ components/index.js
@@ -1,2 +1,5 @@
 export { default as Sidebar } from './Sidebar';
 export { default as SidebarIcons } from './SidebarIcons';
+export { default as Feed } from './Feed';
+export { default as Widgets } from './Widgets';
+export { default as Login } from './Login';
